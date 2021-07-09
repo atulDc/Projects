56458bdc650a353d787ff590ea80c8da63733cf8
atuldc <atul.das13@gmail.com>
2021-07-10 01:06:15 +0530

    init commit

---
+++ ConsumeAPIUsingHttpClient/ConsumeAPIUsingHttpClient/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace ConsumeAPIUsingHttpClient
+{
+    public class Foods
+    {
+        public List<Food> data { get; set; }
+    }
+    public class Food
+    {
+
+        public string city { get; set; }
+        public string name { get; set; }
+        public decimal? estimated_cost { get; set; }
+        public UserRating user_rating { get; set; }
+        public long id { get; set; }
+    }
+
+    public class UserRating
+    {
+        public double? average_rating { get; set; }
+        public long? votes { get; set; }
+    }
+    class Result
+    {
+
+        /*
+         * Complete the 'getTopRatedFoodOutlets' function below.
+         *
+         * URL for cut and paste
+         * https://jsonmock.hackerrank.com/api/food_outlets?city=<city>&page=<pageNumber>
+         *
+         * The function is expected to return an array of strings.
+         * 
+         * The function accepts only city argument (String).
+         */
+        public static async Task<List<string>> getFoodOutletsAsync(string city)
+        {
+            Foods responseObj = new Foods();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://jsonmock.hackerrank.com/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = new HttpResponseMessage();
+            response = await client.GetAsync("api/food_outlets?city=" + city).ConfigureAwait(false);
+            string result = String.Empty;
+            List<string> ls = new List<string>();
+            if (response.IsSuccessStatusCode)
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+                responseObj = JsonConvert.DeserializeObject<Foods>(result);
+            }
+            var maxRating = responseObj.data.Max(x => x.user_rating.average_rating);
+            ls = responseObj.data.Where(x => x.user_rating.average_rating == maxRating).Select(x => x.name + ", " + x.user_rating.average_rating).ToList();
+            return ls;
+        }
+
+        public static List<string> getTopRatedFoodOutlets(string city)
+        {
+            List<string> ls = getFoodOutletsAsync(city).GetAwaiter().GetResult();
+            return ls;
+        }
+
+    }
+    //input: Denver
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine().Trim();
+            List<string> result = Result.getTopRatedFoodOutlets(city);
+            foreach (var name in result)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ps-microservices-getting-started-master/GloboTicket/GloboTicket.Services.EventCatalog/Migrations/20210705142422_init.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GloboTicket.Services.EventCatalog.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("cfb88e29-4744-48c0-94fa-b25b92dea317"),
+                column: "Date",
+                value: new DateTime(2022, 1, 5, 19, 54, 22, 246, DateTimeKind.Local).AddTicks(3559));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("cfb88e29-4744-48c0-94fa-b25b92dea318"),
+                column: "Date",
+                value: new DateTime(2022, 3, 5, 19, 54, 22, 247, DateTimeKind.Local).AddTicks(3599));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("cfb88e29-4744-48c0-94fa-b25b92dea319"),
+                column: "Date",
+                value: new DateTime(2022, 4, 5, 19, 54, 22, 247, DateTimeKind.Local).AddTicks(3524));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("cfb88e29-4744-48c0-94fa-b25b92dea317"),
+                column: "Date",
+                value: new DateTime(2021, 1, 11, 16, 22, 42, 250, DateTimeKind.Local).AddTicks(4932));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("cfb88e29-4744-48c0-94fa-b25b92dea318"),
+                column: "Date",
+                value: new DateTime(2021, 3, 11, 16, 22, 42, 252, DateTimeKind.Local).AddTicks(5838));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("cfb88e29-4744-48c0-94fa-b25b92dea319"),
+                column: "Date",
+                value: new DateTime(2021, 4, 11, 16, 22, 42, 252, DateTimeKind.Local).AddTicks(5753));
+        }
+    }
+}
